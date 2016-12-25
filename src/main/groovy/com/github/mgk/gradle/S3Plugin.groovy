6299431363b5a1be2799bcf0c70505fd543d3014
Michael Keirnan <michael@keirnan.com>
2016-12-24 19:42:19 -0500

    close #2 - implement recursive S3 downloads

--- src/main/groovy/com/github/mgk/gradle/S3Plugin.groovy
+++ src/main/groovy/com/github/mgk/gradle/S3Plugin.groovy
@@ -5,11 +5,17 @@ import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.S3Object
+import com.amazonaws.event.ProgressListener
+import com.amazonaws.event.ProgressEvent
+import com.amazonaws.services.s3.transfer.Transfer
+import com.amazonaws.services.s3.transfer.TransferManager
+
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import java.text.DecimalFormat
 
 
 class S3Extension {
@@ -21,12 +27,6 @@ class S3Extension {
 abstract class S3Task extends DefaultTask {
     @Input
     String bucket
-
-    @Input
-    String key
-
-    @Input
-    String file
 
     String getBucket() { bucket ?: project.s3.bucket }
 
@@ -44,6 +44,12 @@ abstract class S3Task extends DefaultTask {
 
 class S3Upload extends S3Task {
     @Input
+    String key
+
+    @Input
+    String file
+
+    @Input
     boolean overwrite = false
 
     @TaskAction
@@ -56,13 +62,39 @@ class S3Upload extends S3Task {
 
 
 class S3Download extends S3Task {
+    String key
+    String file
+    String keyPrefix
+    String destDir
+
     @TaskAction
     def task() {
-        S3Object s3Object = s3Client.getObject(bucket, key)
-        File f = new File(file)
-        f.parentFile.mkdirs()
-        f.withOutputStream { out ->
-            out << s3Object.objectContent
+        TransferManager tm = new TransferManager()
+        Transfer transfer
+
+        if (keyPrefix != null) {
+            File dir = new File(destDir)
+            dir.mkdirs()
+            transfer = tm.downloadDirectory(bucket, keyPrefix, dir)
+        }
+        else {
+            File f = new File(file)
+            f.parentFile.mkdirs()
+            transfer = tm.download(bucket, key, f)
+        }
+
+        def listener = new S3Listener()
+        listener.transfer = transfer
+        transfer.addProgressListener(listener)
+        transfer.waitForCompletion()
+    }
+
+    class S3Listener implements ProgressListener {
+        Transfer transfer
+
+        DecimalFormat df = new DecimalFormat("#0.0")
+        public void progressChanged(ProgressEvent e) {
+            logger.info("${df.format(transfer.progress.percentTransferred)}%")
         }
     }
 }
